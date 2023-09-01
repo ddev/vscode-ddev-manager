bea0d3c390e5df9fe7e1ee3372ff585b2f61c200
biati-digital <manuel@biati.digital>
2023-09-01 16:53:17 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+
+/** @type WebpackConfig */
+const baseConfig = {
+  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  externals: {
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devtool: "nosources-source-map",
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [{ loader: "ts-loader" }],
+      },
+    ],
+  },
+};
+
+
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    // 'pty': 'node-pty',
+    // 'node-pty': path.resolve(__dirname, "../node-pty"),
+    // 'node-pty': 'commonjs ' + path.resolve("/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/node_modules.asar/node-pty"),
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
+};
+
+
+// Config for webview source code (to be run in a web-based context)
+/** @type WebpackConfig */
+const webviewConfig = {
+  ...baseConfig,
+  target: ["web", "es2020"],
+  entry: "./src/webview/main.ts",
+  experiments: { outputModule: true },
+  output: {
+    path: path.resolve(__dirname, "out"),
+    filename: "webview.js",
+    libraryTarget: "module",
+    chunkFormat: "module",
+  },
+};
+module.exports = [extensionConfig, webviewConfig];
